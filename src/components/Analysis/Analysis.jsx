512dd7e6a24e2ceade524fa62c930f190e931f22
Olga <olga.machulnaya@gmail.com>
2026-05-29 11:43:19 +0300

    Сайт был доработан до соответствия макету: были уточнены визуальные элементы, структура страниц и расположение контента.

--- src/components/Analysis/Analysis.jsx
+++ src/components/Analysis/Analysis.jsx
@@ -30,14 +30,6 @@ function Analysis() {
           <S.CalendarHeaderContainer>
             <S.CalendarHeader>
               <S.CalendarHeaderTitle>Период</S.CalendarHeaderTitle>
-              <S.CalendarFilterLinks>
-                <S.CalendarNavLink type="button" $active>
-                  День
-                </S.CalendarNavLink>
-                <S.CalendarNavLink type="button" $active={false}>
-                  Месяц
-                </S.CalendarNavLink>
-              </S.CalendarFilterLinks>
             </S.CalendarHeader>
             <S.CalendarWeekDays>
               {daysOfWeek.map((day) => (
